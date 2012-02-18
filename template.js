650bf4e683a6fb1ddd1b7b3d86b9eb29078b961b
Mikael Karon <mikael@karon.se>
2012-02-18 13:18:03 +0800

    Added EMPTY instead of ""
    Formatting

--- template.js
+++ template.js
@@ -1,5 +1,5 @@
-/*!
- * TroopJS template
+/*
+ * ! TroopJS template
  * 
  * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
@@ -7,23 +7,25 @@
 /**
  * This plugin provides a template loader and compiler.
  */
-define(["text"], function TemplateModule(text) {
+define([ "text" ], function TemplateModule(text) {
 	var RE_SANITIZE = /^[\n\t\r]+|[\n\t\r]+$/g;
 	var RE_BLOCK = /<%(=)?([\S\s]*?)%>/g;
 	var RE_TOKENS = /<%(\d+)%>/gm;
 	var RE_REPLACE = /(["\n\t\r])/gm;
 	var RE_CLEAN = /o \+= "";| \+ ""/gm;
+	var EMPTY = "";
 	var REPLACE = {
-		"\"": "\\\"",
-		"\n": "\\n",
-		"\t": "\\t",
-		"\r": "\\r"
+		"\"" : "\\\"",
+		"\n" : "\\n",
+		"\t" : "\\t",
+		"\r" : "\\r"
 	};
 
 	var buildMap = {};
 
 	/**
 	 * Compiles template
+	 * 
 	 * @param body Template body
 	 * @returns {Function}
 	 */
@@ -31,15 +33,14 @@ define(["text"], function TemplateModule(text) {
 		var blocks = [];
 		var length = 0;
 
-		function blocksTokens (original, prefix, block) {
+		function blocksTokens(original, prefix, block) {
 			blocks[length] = prefix
 				? "\" +" + block + "+ \""
 				: "\";" + block + "o += \"";
-
 			return "<%" + String(length++) + "%>";
 		}
 
-		function tokensBlocks (original, token) {
+		function tokensBlocks(original, token) {
 			return blocks[token];
 		}
 
@@ -48,27 +49,27 @@ define(["text"], function TemplateModule(text) {
 		}
 
 		return new Function("data", ("var o; o = \""
-			// Sanitize body before we start templating
-			+ body.replace(RE_SANITIZE, "")
+		// Sanitize body before we start templating
+		+ body.replace(RE_SANITIZE, "")
 
-			// Replace script blocks with tokens
-			.replace(RE_BLOCK, blocksTokens)
+		// Replace script blocks with tokens
+		.replace(RE_BLOCK, blocksTokens)
 
-			// Replace unwanted tokens
-			.replace(RE_REPLACE, replace)
+		// Replace unwanted tokens
+		.replace(RE_REPLACE, replace)
 
-			// Replace tokens with script blocks
-			.replace(RE_TOKENS, tokensBlocks)
+		// Replace tokens with script blocks
+		.replace(RE_TOKENS, tokensBlocks)
 
-			+ "\"; return o;")
+		+ "\"; return o;")
 
-			// Clean
-			.replace(RE_CLEAN, ""));
+		// Clean
+		.replace(RE_CLEAN, EMPTY));
 	}
 
 	return {
-		load: function (name, req, load, config) {
-			text.load(name, req, function (value) {
+		load : function(name, req, load, config) {
+			text.load(name, req, function(value) {
 				// Compile template and store in buildMap
 				var template = buildMap[name] = compile(value);
 
@@ -80,9 +81,9 @@ define(["text"], function TemplateModule(text) {
 			}, config);
 		},
 
-		write: function (pluginName, moduleName, write, config) {
+		write : function(pluginName, moduleName, write, config) {
 			if (moduleName in buildMap) {
-				write.asModule(pluginName + "!" + moduleName, "define(function () { return " + buildMap[moduleName].toString().replace(RE_SANITIZE, "") + ";});\n");
+				write.asModule(pluginName + "!" + moduleName, "define(function () { return " + buildMap[moduleName].toString().replace(RE_SANITIZE, EMPTY) + ";});\n");
 			}
 		}
 	};
