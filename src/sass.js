d3ed7ace6ecf80098adbc50d0af01e334e15e9d0
Mikael Karon <mikael@karon.se>
2012-05-18 09:49:01 +0800

    added initial sass plugin

---
+++ src/sass.js
@@ -0,0 +1,37 @@
+/*!
+ * TroopJS RequireJS sass plug-in
+ * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
+ * Released under the MIT license.
+ */
+/**
+ * This plugin provides a .sass loader that uses sass.js to compile.
+ */
+define([ "text", "sassjs" ], function SassModule(text, sassjs) {
+	var RE_REPLACE = /(["\n\t\r])/gm;
+	var REPLACE = {
+		"\"" : "\\\"",
+		"\n" : "\\n",
+		"\t" : "\\t",
+		"\r" : "\\r"
+	};
+
+	var buildMap = {};
+
+	function replace(original, token) {
+		return REPLACE[token] || token;
+	}
+
+	return {
+		load : function(name, req, load, config) {
+			text.load(name, req, function(value) {
+				load(buildMap[name] = sassjs.render(value));
+			}, config);
+		},
+
+		write : function(pluginName, moduleName, write, config) {
+			if (moduleName in buildMap) {
+				write.asModule(pluginName + "!" + moduleName, "define(function () { return \"" + buildMap[moduleName].replace(RE_REPLACE, replace) + "\"; });\n");
+			}
+		}
+	};
+});
